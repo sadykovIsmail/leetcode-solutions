64f47ac8ce4999afd43bb1cd5b769c0476a38e5e
Ismail Sadykov <ghvh8176@gmail.com>
2026-03-25 12:58:00 -0500

    solve: solve the problem design circular queue Closes #6

--- Python/5. queue_stack/1. design_circular_queue.py
+++ Python/5. queue_stack/1. design_circular_queue.py
@@ -1,32 +1,41 @@
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        
+        self.queue = [0] * k
+        self.head = 0
+        self.tail = 0
+        self.size = k
+        self.count = 0   # number of elements
 
     def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
         
+        self.queue[self.tail] = value
+        self.tail = (self.tail + 1) % self.size
+        self.count += 1
+        return True
 
     def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
         
+        self.head = (self.head + 1) % self.size
+        self.count -= 1
+        return True
 
     def Front(self) -> int:
-        
+        if self.isEmpty():
+            return -1
+        return self.queue[self.head]
 
     def Rear(self) -> int:
-        
+        if self.isEmpty():
+            return -1
+        return self.queue[(self.tail - 1) % self.size]
 
     def isEmpty(self) -> bool:
-        
+        return self.count == 0
 
     def isFull(self) -> bool:
-        
-
-
-# Your MyCircularQueue object will be instantiated and called as such:
-# obj = MyCircularQueue(k)
-# param_1 = obj.enQueue(value)
-# param_2 = obj.deQueue()
-# param_3 = obj.Front()
-# param_4 = obj.Rear()
-# param_5 = obj.isEmpty()
-# param_6 = obj.isFull()
+        return self.count == self.size
