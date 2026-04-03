2ea1aa7ce0275737d74036e03aad0b1fb2ee3d74
Ismail Sadykov <ghvh8176@gmail.com>
2026-04-03 12:27:37 -0500

    feat: traverse binary tree iteratively

--- Python/5. queue_stack/stack/dfs/2. binary tree inorder traversal.py
+++ Python/5. queue_stack/stack/dfs/2. binary tree inorder traversal.py
@@ -1,3 +1,4 @@
+from typing import List, Optional
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
@@ -19,4 +20,29 @@ class Solution:
         # Kick off the recursion starting at the root
         dfs(root)
         
+        return res
+    
+# implemented iteratively
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        curr = root  # We use a pointer to represent where we are standing
+        
+        # Keep running if we are standing in a valid room (curr) 
+        # OR if we have sticky notes left in our memory (stack)
+        while curr or stack:
+            
+            # 1. Dive as far Left as humanly possible, saving breadcrumbs
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+                
+            # 2. We hit a dead end (curr is None). Eject and process!
+            curr = stack.pop()
+            res.append(curr.val)
+            
+            # 3. Step through the Right door (even if it doesn't exist)
+            curr = curr.right
+            
         return res
